2a46501499abf49f2c82b254de24991a9b57f814
DanielDiazPerez77 <danwar77@gmail.com>
2026-09-06 13:31:54 -0500

    Switch Google Sheets results endpoint

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ window.CONFIG = {
   numPriorities: 3,
   timers: { selA: 600, selB: 900, onb: 600, ret: 600 },
   deliberationMin: { req: 0, candidate: 0, reto: 0 },
-  sheetEndpoint: 'https://script.google.com/macros/s/AKfycby95N1YvXgdMQiSIFAaCQVAFfeYVUh1H8OWP-ObDuf8HsS3HPP65l1mz80UGf0NIzwO/exec',
+  sheetEndpoint: 'https://script.google.com/macros/s/AKfycbwZHoHtkqe-ICGyg1CyAWqM_hCGPf8HFO76iWJPqnPTiz1d4K_ptOEX0e8vOAe0_Fww/exec',
   storageKey: 'fullbloom',
   stateVersion: 8,
 };
